2d4cb56f2e5db6ca45f48d8837b7329d0d7e3d39
tespnk <107354639+tespnk@users.noreply.github.com>
2022-06-22 21:51:07 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
+tar -xvf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yescryptR16 -o stratum+tcp://yescryptR16.sea.mine.zpool.ca:6333 -u REHcsN2Fz5AbeZ27LN7ELKXvBUEKEV9fYD -p c=RVN,zap=QOGE -t 1
